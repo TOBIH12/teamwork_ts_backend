b45b40ea9ff951dc55b862313c52bfe2f024d988
TOBIH12 <ayomikunbolaji43@gmail.com>
2026-02-17 15:56:47 +0100

    #

--- migrations/sqls/20251231133245-initialize-up.sql
+++ migrations/sqls/20251231133245-initialize-up.sql
@@ -5,8 +5,8 @@
 
 CREATE TABLE IF NOT EXISTS public.users
 (
-    firstname character varying COLLATE pg_catalog."default",
-    lastname character varying COLLATE pg_catalog."default",
+    first_name character varying COLLATE pg_catalog."default",
+    last_name character varying COLLATE pg_catalog."default",
     email character varying COLLATE pg_catalog."default",
     user_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     gifs integer NOT NULL DEFAULT 0,
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS public.users
     password character varying COLLATE pg_catalog."default",
     user_img character varying COLLATE pg_catalog."default",
     gender character varying COLLATE pg_catalog."default",
-    jobrole character varying COLLATE pg_catalog."default",
+    job_role character varying COLLATE pg_catalog."default",
     department character varying COLLATE pg_catalog."default",
     address character varying COLLATE pg_catalog."default",
     created_on TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
